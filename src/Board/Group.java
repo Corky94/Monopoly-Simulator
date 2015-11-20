9ec9a516ee7a4cd67fa8b508e9d44fcf93825732
Marc Cork <Marc.Cork@gmail.com>
2015-11-20 12:32:02 +0000

    Made changes to the group enum to incorperate, tax jail and go.

--- src/Board/Group.java
+++ src/Board/Group.java
@@ -4,5 +4,5 @@ package Board;
  * Created by marc on 20/11/2015.
  */
 public enum Group {
-    Purple,LightGreen,Violet,Orange,Red,Yellow,DarkGreen,Utilities,RailRoad
+    Purple,LightGreen,Violet,Orange,Red,Yellow,DarkGreen,Utilities,RailRoad,GO,Tax,Jail,GoToJail
 }
