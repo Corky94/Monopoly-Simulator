e63f9a687cb5c3600ee01513316adfd295ff69fe
Marc Cork <marc.cork@gmail.com>
2016-02-23 11:12:39 +0000

    Auction Rules implemented and tested in Lua

--- src/main/java/Rules/AuctionRules.java
+++ src/main/java/Rules/AuctionRules.java
@@ -1,26 +1,41 @@
 package Rules;
 
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
 /**
  * Created by userhp on 29/01/2016.
  */
 public class AuctionRules {
     private static  AuctionRules instance = new AuctionRules();
-    private static double startingPirceMultipler;
-    private static double incrementMultipler;
+    private static double startingPriceMultiplier;
+    private static double incrementMultiplier;
     private AuctionRules(){}
     public static AuctionRules getInstance() {
         return instance;
     }
-    public static void init(double startingPriceMultiplerInit, double incrementMultiplerInit){
-        startingPirceMultipler = startingPriceMultiplerInit;
-        incrementMultipler = incrementMultiplerInit;
+
+    public static void init(double startingPriceMultiplierInit, double incrementMultiplierInit) {
+        startingPriceMultiplier = startingPriceMultiplierInit;
+        incrementMultiplier = incrementMultiplierInit;
     }
 
+    LuaValue _G;
+
+    public AuctionRules(String luaFileLocation) {
+        _G = JsePlatform.standardGlobals();
+        _G.get("dofile").call(LuaValue.valueOf(luaFileLocation));
+    }
+
+
+
     public double getStartingPriceMultiplier() {
-        return startingPirceMultipler;
+        LuaValue getStartingPriceMultiplier = _G.get("getStartingPriceMultiplier");
+        return getStartingPriceMultiplier.call().todouble();
     }
 
     public double getIncrementMultiplier() {
-        return incrementMultipler;
+        LuaValue getIncrementMultiplier = _G.get("getIncrementMultiplier");
+        return getIncrementMultiplier.call().todouble();
     }
 }
